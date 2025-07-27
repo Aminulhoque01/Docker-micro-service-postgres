7b98193315c511071964ed996ea04de5cbaba010
Aminulhoque01 <aminuhoque53@gmail.com>
2025-07-27 19:26:19 +0600

    ok

--- api-gateway/src/utils.ts
+++ api-gateway/src/utils.ts
@@ -1,28 +1,65 @@
 
-import {Express, Request,Response} from "express";
+// import {Express, Request,Response} from "express";
+// import config from './config.json';
+// import axios from "axios";
+
+
+// export const createHandler = (hostname:string, path:string, method:string)=>{
+//     return async(req:Request, res:Response)=>{
+//   const {data}= await axios({
+//     method,
+//     url:`${hostname}${path}`,
+//     data: req.body
+//   })
+//   res.json(data)
+// }}
+
+// export const configureRoutes= (app:Express)=>{
+//  Object.entries(config.services).forEach(([name,service])=>{
+//     const hostname= service.url;
+//     service.routes.forEach((route)=>{
+//         route.methods.forEach((method)=>{
+//            const handler = createHandler(hostname, route.path, method);
+//            app[method](`/api/${route.path}`, handler)
+//         })
+//     })
+     
+//  })
+// }
+
+
+import { Express, Request, Response } from "express";
 import config from './config.json';
 import axios from "axios";
 
+type ExpressMethod = 'get' | 'post' | 'put' | 'delete' | 'patch' | 'options' | 'head';
 
-export const createHandler = (hostname:string, path:string, method:string)=>{
-    return async(req:Request, res:Response)=>{
-  const {data}= await axios({
-    method,
-    url:`${hostname}${path}`,
-    data: req.body
-  })
-  res.json(data)
-}}
+export const createHandler = (hostname: string, path: string, method: string) => {
+    return async (req: Request, res: Response) => {
+        const { data } = await axios({
+            method,
+            url: `${hostname}${path}`,
+            data: req.body
+        });
+        res.json(data);
+    };
+};
 
-export const configureRoutes= (app:Express)=>{
- Object.entries(config.services).forEach(([name,service])=>{
-    const hostname= service.url;
-    service.routes.forEach((route)=>{
-        route.methods.forEach((method)=>{
-           const handler = createHandler(hostname, route.path, method);
-           app[method](`/api/${route.path}`, handler)
-        })
-    })
-     
- })
-}
+export const configureRoutes = (app: Express) => {
+    const validMethods: ExpressMethod[] = ['get', 'post', 'put', 'delete', 'patch', 'options', 'head'];
+
+    Object.entries(config.services).forEach(([name, service]) => {
+        const hostname = service.url;
+        service.routes.forEach((route) => {
+            route.methods.forEach((method: string) => {
+                const expressMethod = method.toLowerCase() as ExpressMethod;
+                if (validMethods.includes(expressMethod)) {
+                    const handler = createHandler(hostname, route.path, method);
+                    app[expressMethod](`/api/${route.path}`, handler);
+                } else {
+                    console.error(`Invalid HTTP method: "${method}" for route ${route.path} in service ${name}`);
+                }
+            });
+        });
+    });
+};
