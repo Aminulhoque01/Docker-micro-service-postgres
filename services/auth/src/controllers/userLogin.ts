f024a8719e4ec9315c0f198f73cc0c91f7b16aeb
Aminulhoque01 <aminuhoque53@gmail.com>
2025-08-03 00:55:42 +0600

    accesstoken

--- services/auth/src/controllers/userLogin.ts
+++ services/auth/src/controllers/userLogin.ts
@@ -2,13 +2,20 @@ import { NextFunction, Request, Response } from "express";
 import { UserLoginSchema } from "../schema";
 import prisma from "../prisma";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 
-const userLogin = async (req:Request, res:Response, next:NextFunction) => {
+const userLogin = async (req: Request, res: Response, next: NextFunction) => {
     try {
+
+        // history tracking
+        // You can implement a history tracking mechanism here if needed
+        const ipAddress = req.headers['x-forwarded-for'] || req.ip || "";
+        const userAgent = req.headers['user-agent'] || "";
+
         // Validate request body
         const parsedBody = UserLoginSchema.safeParse(req.body);
-        if (!parsedBody.success) {  
+        if (!parsedBody.success) {
             return res.status(400).json({
                 message: parsedBody.error.issues,
             });
@@ -51,8 +58,34 @@ const userLogin = async (req:Request, res:Response, next:NextFunction) => {
             });
         }
 
-        const ipAddress = req.headers['x-forwarded-for'] || req.ip || "";
-        const userAgent = req.headers['user-agent'] || "";
+        //check if user is active
+        if (user.status !== "ACTIVE") {
+            return res.status(403).json({
+                message: `User is not active, current status: ${user.status.toLocaleLowerCase()}`,
+            });
+        }
+
+        //ganarate JWT token
+        const accessToken = jwt.sign(
+            { userId: user.id, email: user.email, name: user.name, role: user.role },
+            process.env.JWT_SECRET || "default_secret",
+            { expiresIn: "1h" }
+        )
+
+        return res.status(200).json({
+            message: "User logged in successfully",
+            accessToken,
+            user: {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                role: user.role,
+                status: user.status,
+                createdAt: user.createdAt,
+                verified: user.verified,
+            }
+        });
+
     } catch (error) {
         next(error);
     }
