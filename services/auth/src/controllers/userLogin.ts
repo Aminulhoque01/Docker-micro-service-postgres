2500dc7b3f397b31a291182d9b819a3ce2308f4d
Aminulhoque01 <aminuhoque53@gmail.com>
2025-08-03 00:10:14 +0600

    login route

--- services/auth/src/controllers/userLogin.ts
+++ services/auth/src/controllers/userLogin.ts
@@ -1,10 +1,58 @@
 import { NextFunction, Request, Response } from "express";
-
+import { UserLoginSchema } from "../schema";
+import prisma from "../prisma";
+import bcrypt from "bcryptjs";
 
 
 const userLogin = async (req:Request, res:Response, next:NextFunction) => {
     try {
-        
+        // Validate request body
+        const parsedBody = UserLoginSchema.safeParse(req.body);
+        if (!parsedBody.success) {  
+            return res.status(400).json({
+                message: parsedBody.error.issues,
+            });
+        }
+        // Check if user exists
+        const user = await prisma.user.findUnique({
+            where: {
+                email: parsedBody.data.email,
+            },
+            select: {
+                id: true,
+                email: true,
+                password: true, // Include password for comparison
+                name: true,
+                role: true,
+                status: true,
+                createdAt: true,
+                verified: true,
+            }
+        })
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        // compare passwords
+        const isPasswordValid = await bcrypt.compare(parsedBody.data.password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        //check if user is verified
+        if (!user.verified) {
+            return res.status(403).json({
+                message: "User is not verified",
+            });
+        }
+
+        const ipAddress = req.headers['x-forwarded-for'] || req.ip || "";
+        const userAgent = req.headers['user-agent'] || "";
     } catch (error) {
         next(error);
     }
--- services/auth/src/schema.ts
+++ services/auth/src/schema.ts
@@ -7,3 +7,8 @@ export const UserCreateSchema = z.object({
    password: z.string().min(8, "Password must be at least 8 characters  long"),
    name: z.string().min(3).max(100)  
 })
+
+export const UserLoginSchema = z.object({
+   email: z.string().email(),
+   password: z.string().min(8, "Password must be at least 8 characters long"),
+});
